3688fbbb910753695e3b8de249c77595ec794ec3
alu0100406122 <alu0100406122@ull.edu.es>
2016-02-22 10:26:57 +0000

    Modificación de los enlaces

--- js/dsi_practicas.js
+++ js/dsi_practicas.js
@@ -4,9 +4,9 @@
         {
             "id" : 1,
             "titulo": "Tareas Iniciales",
-            "imagen": "img/practica1.png",
-            "url": "http://github.com/JosueTC94/tareas-iniciales-josue-nayra-dsi15-16/",
-            "url_paginaweb": "http://alu0100406122.github.io/tareas-iniciales-josue-nayra-dsi15-16/"
+            "imagen": "img/portfolio/roundicons.png",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/tareas-iniciales-josue-nayra-dsi15-16",
+            "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/tareas-iniciales-josue-nayra-dsi15-16/"
         }
     ]
 }
