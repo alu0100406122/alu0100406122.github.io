5c826496f2c787b5df96d42c1797e6d6315e396f
alu0100406122 <alu0100406122@ull.edu.es>
2016-03-28 08:23:46 +0000

    Añadiendo nuevas prácticas a la asignatura DSI.

--- js/dsi_practicas.js
+++ js/dsi_practicas.js
@@ -14,6 +14,27 @@
             "imagen": "img/termometro.png",
             "url": "https://github.com/ULL-ESIT-GRADOII-DSI/introduccion-josue-nayra-dsi15-16/tree/gh-pages",
             "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/introduccion-josue-nayra-dsi15-16/"
+        },
+        {
+            "id" : 3,
+            "titulo": "Conversor de Temperatura. OOP.",
+            "imagen": "img/termometro.png",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/introduccion-josue-nayra-dsi15-16/tree/gh-pages",
+            "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/introduccion-josue-nayra-dsi15-16/"
+        },
+        {
+            "id" : 4,
+            "titulo": "Conversor de Temperatura. Eliminación del Switch Smells.",
+            "imagen": "img/smells.jpg",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/eliminacion-del-switch-smell-josue-nayra-dsi15-16-1",
+            "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/eliminacion-del-switch-smell-josue-nayra-dsi15-16-1/"
+        },
+        {
+            "id" : 5,
+            "titulo": "Conversor de Temperatura. Pruebas Mocha y Chai.",
+            "imagen": "img/mocha.png",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/mocha-y-chai-josue-nayra-dsi15-16-1",
+            "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/mocha-y-chai-josue-nayra-dsi15-16-1/vendor/index.html"
         }
     ]
 }
